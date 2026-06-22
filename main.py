220aabac74c3a3f0ae60475d4318485e26552de7
notkwei <257144024+notkwei@users.noreply.github.com>
2026-06-21 22:41:46 -0500

    Implement resource pack custom model data, basic resource pack generation now complete.

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ datapack_output_path: Path =			Path("data_pack")
 
 #Settings ()
 generate_bedrock_path: bool = False
+music_disc_item_string: str = "music_disc_wait"
 java_resource_pack_format: float = 88.0 # Microsoft, why. Just. WHY. IT IS A VERSION NUMBER. MAKE IT A NUMBER. NOT A FLOAT.
 datapack_format: float = 107.2	# *throws up*
 resource_pack_description : str = "Adds music discs to the game."
@@ -134,15 +135,26 @@ pack_mcmeta = {"pack": {"pack_format": java_resource_pack_format,
 write_dict_to_json(pack_mcmeta, resource_pack_output_java_folder / "pack.mcmeta")
 
 Path(resource_pack_output_java_folder / "assets" / pack_id / "models" / "item").mkdir(parents=True, exist_ok=True)
+Path(resource_pack_output_java_folder / "assets/minecraft/models/item").mkdir(parents=True, exist_ok=True)
 
 sounds_json = {}
+model_data_mappings = {"parent": "item/generated",
+						   "textures": {
+							   "layer0": f"item/{music_disc_item_string}"
+						   },
+						   "overrides": []
+						   }
 
 for disc in audio_files:
 	disc_id = audio_files[disc]["id_string"]
+	disc_custom_model_id = int(audio_files[disc]["custom_model_data"])
 	print("[*] Processing disc", disc_id)
 
 	sounds_json["music_disc." + disc_id] = {"sounds": [{"name": pack_id + ":records/" + disc_id,
-														"stream": True}]} # formats sounds.json as music_disc.id_string = {"sounds"
+														"stream": True}]} # formats sounds.json as music_disc.id_string = {"sounds"...
+	model_data_mappings["overrides"].append({"predicate": { "custom_model_data": disc_custom_model_id }, "model": f"{pack_id}:item/music_disc_{disc_id}"}) # I promise at one point in time this looked nice
+
+
 	item_json = {"parent": "item/generated",
 				 "textures": {
 					 "layer0": pack_id + ":item/" + disc_id
@@ -150,7 +162,7 @@ for disc in audio_files:
 	write_dict_to_json(item_json, resource_pack_output_java_folder / "assets" / pack_id / "models" / "item" / f"music_disc_{disc_id}.json")
 
 write_dict_to_json(sounds_json, resource_pack_output_java_folder / "assets" / pack_id / "sounds.json")
-
+write_dict_to_json(model_data_mappings, resource_pack_output_java_folder / "assets/minecraft/models/item/" / f"{music_disc_item_string}.json")
 
 
 #assets/disco/sounds/records/discname.ogg
